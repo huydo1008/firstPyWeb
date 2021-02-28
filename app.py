d8d11984788ebba28aec77402c60ad104269b9b2
huydo <huydo1008@gmail.com>
2021-02-28 23:33:33 +0700

    added index.html

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+
+def main():
+    return render_template('index.html')
+
 def hello():
     return "Hello HUY DO!"
